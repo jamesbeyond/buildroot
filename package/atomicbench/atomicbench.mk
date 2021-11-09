9ecf833872744ba1278de031bd119c845b7c07e8
Wang Junqiang <wangjunqiang.geass@gmail.com>
2021-11-09 12:11:24 +0800

    configs: fix build misc bug

--- package/atomicbench/atomicbench.mk
+++ package/atomicbench/atomicbench.mk
@@ -4,11 +4,11 @@ ATOMICBENCH_LICENSE = Apache-2.0
 ATOMICBENCH_LICENSE_FILES = LICENSE.md
 
 define ATOMICBENCH_BUILD_CMDS
- $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+ $(TARGET_MAKE_ENV) CROSS_COMPILE=$(notdir $(TARGET_CROSS:%-=%)) $(MAKE) -C $(@D)
 endef
 
 define ATOMICBENCH_INSTALL_TARGET_CMDS
- $(INSTALL) -D $(@D)/build/benchmark/$(CROSS_COMPILE)/main.bin $(TARGET_DIR)/usr/bin/atomicbench
+ $(INSTALL) -D $(@D)/build/benchmark/$(notdir $(TARGET_CROSS:%-=%))/main.bin $(TARGET_DIR)/usr/bin/atomicbench
 endef
 
 $(eval $(generic-package))
--- package/pts-ci/pts-ci.mk
+++ package/pts-ci/pts-ci.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 define PTS_CI_INSTALL_TARGET_CMDS
-	cp -f ./package/pst-ci/pts_run $(TARGET_DIR)/etc/init.ci/
+	cp -f ./package/pts-ci/pts_run $(TARGET_DIR)/etc/init.ci/
 	chmod a+x $(TARGET_DIR)/etc/init.ci/pts_run
 endef
 
