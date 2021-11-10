2caaeb60ccc20ffea0f5380060ac3b856d20986a
Wang Junqiang <wangjunqiang.geass@gmail.com>
2021-11-10 11:06:23 +0800

    configs:add pts parse for test

--- package/pts-ci/pts-ci.mk
+++ package/pts-ci/pts-ci.mk
@@ -7,6 +7,8 @@
 define PTS_CI_INSTALL_TARGET_CMDS
 	cp -f ./package/pts-ci/pts_run $(TARGET_DIR)/etc/init.ci/
 	chmod a+x $(TARGET_DIR)/etc/init.ci/pts_run
+	cp -f ./package/pts-ci/pts_parse $(HOST_DIR)/csky-ci/parse_script/
+	chmod a+x $(HOST_DIR)/csky-ci/parse_script/pts_parse
 endef
 
 $(eval $(generic-package))
