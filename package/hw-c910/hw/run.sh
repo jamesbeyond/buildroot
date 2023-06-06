c4ff0e3f8f66b820d68d2384436fda28402f67e7
Qinghao Shi <shiqinghao.sqh@alibaba-inc.com>
2023-06-06 09:46:44 +0800

    add xiaohui platfrom and update run.sh
    Signed-off-by: Qinghao Shi <shiqinghao.sqh@alibaba-inc.com>

--- package/hw-c910/hw/run.sh
+++ package/hw-c910/hw/run.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
+
 if [ $# -lt 1 -o $# -gt 4 ] ; then
-	echo "Usage: . run.sh <ip:port> [an/eg/eg2/ve/ice_evb/ice_fpga/ice_fpga2/th] [s/m] noreset"
-	echo "Usage: [an/eg/eg2/ve/ice_evb/ice_fpga/ice_fpga2/th] is for board name"
-	echo "Usage: [s/m] is for onecore/twocore"
+	echo "Usage:  run.sh <DebugServer_ip:port>  <soc_platform>  <core_pattern>"
+	echo ""
+	echo "<soc_platform>:  Choose one from [ ice_fpga2 | x908 | xiaohui ] etc."
+	echo "<core_pattern> :  binary format, each bit per core, 10011 mean enable core 0,1,4"
 	exit 1
 fi
 
-BOARD="eg"
-NRCORE=2
 
-GDBRESET="reset"
-if [ "$4" == "noreset" ]; then
+
 GDBRESET="noreset"
-fi
 
 for idx in "$@"
 do
@@ -51,6 +49,9 @@ elif [ $idx == "ice_fpga" ]; then
 elif [ $idx == "ice_fpga2" ]; then
 	BOARD="ice_fpga2"
 	GDBRESET="noreset"
+elif [ $idx == "xiaohui" ]; then
+	BOARD="xiaohui"
+	GDBRESET="noreset"
 elif [ $idx == "x908" ]; then
 	BOARD="x908"
 	GDBRESET="noreset"
@@ -66,42 +67,48 @@ elif [ $idx == "eg2" ]; then
 elif [ $idx == "ve" ]; then
 	BOARD="ve"
 	GDBRESET="noreset"
-elif [ $idx == "s" ]; then
-	NRCORE=1
-elif [ $idx == "m" ]; then
-	NRCORE=2
 fi
-case "$idx" in
-    [1-9] | [1-1][0-6])
-		NRCORE=$idx
-esac
 done
 
-set -ex
+NRCORE=$3
 
-DDRINIT=$BOARD.socinit.txt
+set -e
+
+SOCINIT=$BOARD.socinit.txt
 GDBINIT=$BOARD.gdbinit.txt
 DTS=$BOARD.dts.txt
 
-echo "Use config" $NRCORE $BOARD
-if [ ! -f $GDBINIT -o ! -f $DTS ]; then
-	echo "No support"
-	exit 1
+echo "======== launch info ========"
+echo "platform:" $BOARD
+echo "socinit :" $SOCINIT
+echo "gdbinit :" $GDBINIT
+echo "dts     :" $DTS
+echo "cores   :" $NRCORE
+echo "==== ==== ==== ==== ==== ===="
+if [ ! -f $GDBINIT -o ! -f $SOCINIT -o ! -f $DTS ]; then
+		echo "Error: init file or dts not exist !"
+		exit 1
 fi
 
 enable_dts_cores() {
-	let x=$1-1
-	for ((i=1;i<=$x;i++)); do
+
+	binary=$1
+	decimal=$((2#$binary))
+	length=${#binary}
+
+	for (( i=0; i<${length}; i++ )); do
+	if ((decimal & 1 == 1)); then
 		y=`awk '/cpu@'$i' \{/{getline a;print NR}' .hw.dts`
 		let y=$y+2
 		sed -i ''$y','$y's/fail/okay/g' .hw.dts
+	fi
+	decimal=$((decimal >> 1))
 	done
 }
 
+# patching device tree file
 cp $DTS .hw.dts
-if [ $NRCORE -gt 1 ] ; then
 enable_dts_cores $NRCORE
-fi
 
 ROOTFS_BASE=`cat .hw.dts | grep initrd-start | awk -F " " '{print $4}' | awk -F ">" '{print $1}'`
 ROOTFS_SIZE=`ls -lt ../rootfs.cpio.gz | awk '{print $5}'`
@@ -109,6 +116,8 @@ ROOTFS_SIZE=`ls -lt ../rootfs.cpio.gz | awk '{print $5}'`
 ROOTFS_END=`printf "0x%x" $ROOTFS_END`
 sed -i "s/linux,initrd-end = <0x0 .*/linux,initrd-end = <0x0 $ROOTFS_END>;/g" .hw.dts
 
+set -x
+# compile device tree file
 ./dtc -I dts -O dtb .hw.dts > hw.dtb
 
 # reset
@@ -116,12 +125,12 @@ if [ $GDBRESET == "reset" ]; then
 ./riscv64-elf-gdb -ex "tar remote $1" -ex "reset" -ex "set confirm off" -ex "q"
 fi
 
-# Init DDR
+# SoC related initialization
 if [ $BOARD == "eg" -o $BOARD == "eg2" ]; then
-./riscv64-elf-gdb -ex "tar remote $1" -x $DDRINIT ddr_init_eg_elf -ex "c" -ex "q" > /dev/null
+./riscv64-elf-gdb -ex "tar remote $1" -x $SOCINIT ddr_init_eg_elf -ex "c" -ex "q" > /dev/null
 else
-./riscv64-elf-gdb -ex "tar remote $1" -x $DDRINIT -ex "q"
+./riscv64-elf-gdb -ex "tar remote $1" -x $SOCINIT -ex "q"
 fi
 
-# Run linux
+# boot linux with gdbinit
 ./riscv64-elf-gdb -ex "tar remote $1" -x $GDBINIT -ex "c" -ex "q"
